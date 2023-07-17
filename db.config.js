618053e148045cce8bceef0c7b4655498108f778
boommali <139744700+boommali@users.noreply.github.com>
2023-07-17 14:39:39 +0700

    Add files via upload

---
+++ db.config.js
@@ -0,0 +1,29 @@
+// module.exports = {
+//     HOST: "dpg-ch7nrpak728iqr621qug-a",
+//     USER: "grady",
+//     PORT: 5432,
+//     PASSWORD: "KLDR2dt4eeQF7I2CR6i0Ls6rzXqjWvBd",
+//     DB: "boombooom",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
+//
+module.exports = {
+    HOST: "localhost",
+    USER: "postgres",
+    PORT: 5432,
+    PASSWORD: "postgres",
+    DB: "boomboom",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
