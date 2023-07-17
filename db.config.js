62efdec4d1054bf814154c1d9239f082563aad3a
boommali <139744700+boommali@users.noreply.github.com>
2023-07-17 14:42:58 +0700

    Add files via upload

--- db.config.js
+++ db.config.js
@@ -1,24 +1,9 @@
-// module.exports = {
-//     HOST: "dpg-ch7nrpak728iqr621qug-a",
-//     USER: "grady",
-//     PORT: 5432,
-//     PASSWORD: "KLDR2dt4eeQF7I2CR6i0Ls6rzXqjWvBd",
-//     DB: "boombooom",
-//     dialect: "postgres",
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }
-// };
-//
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
+    HOST: "dpg-ciqeefl9aq0dcpt706r0-a",
+    USER: "grady",
     PORT: 5432,
-    PASSWORD: "postgres",
-    DB: "boomboom",
+    PASSWORD: "jxeWS71FfqOKpiZIOj0zoJJiZwdaQwZH",
+    DB: "bmdb_rpcq",
     dialect: "postgres",
     pool: {
         max: 5,
@@ -27,3 +12,18 @@ module.exports = {
         idle: 10000
     }
 };
+
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PORT: 5432,
+//     PASSWORD: "postgres",
+//     DB: "boomboom",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
